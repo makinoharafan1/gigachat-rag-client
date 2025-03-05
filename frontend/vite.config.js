04faac6723041bb400307694b130b51fbd75f8e4
makinoharafan1 <niugnep123lol@gmail.com>
2025-03-05 22:13:51 +0600

    Initial

---
+++ frontend/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig, loadEnv } from 'vite'
+
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      react()
+    ],
+    server: {
+      host: true,
+      port: env.VITE_PORT,
+      watch: {
+        usePolling: true
+      },
+      esbuild: {
+        target: "esnext",
+        platform: "linux",
+      }
+    }
+  }
+})
